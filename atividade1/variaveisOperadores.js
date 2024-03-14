cca501760ff2d5a41046e0209627fbe64474eb88
duduantero <xduduanterox@gmail.com>
2024-03-14 19:59:40 -0300

    first commit

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,14 @@
+let num_a = 22;
+let numb_b = 5;
+
+let resultado = num_a + numb_b;
+console.log('Resultado da soma entre ' + num_a + ' e ' + numb_b + ' é ' + resultado);
+
+resultado = num_a - numb_b;
+console.log(`Resultado da subtração entre ${num_a} e ${numb_b} é ${resultado}`);
+
+resultado = num_a * numb_b;
+console.log(`Resultado da multiplicação entre ${num_a} e ${numb_b} é ${resultado}`);
+
+resultado = num_a / numb_b;
+console.log(`Resultado da divisão entre ${num_a} e ${numb_b} é ${resultado}`);
